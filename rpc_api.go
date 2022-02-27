6c684cd6ed523b46de14b67762c5e1a26ad47d52
Taylor Arnett <tarnett1@mac.com>
2022-02-27 15:01:34 -0700

    first commit

---
+++ rpc_api.go
@@ -0,0 +1,22 @@
+package main
+
+import "context"
+
+const (
+	EchoService         = "EchoRPCAPI"
+	EchoServiceFuncEcho = "Echo"
+)
+
+type EchoRPCAPI struct {
+	service *Service
+}
+
+type Envelope struct {
+	Message string
+}
+
+func (r *EchoRPCAPI) Echo(ctx context.Context, in Envelope, out *Envelope) error {
+	// RPC method
+	*out = r.service.ReceiveEcho(in)
+	return nil
+}
