16b6d0275c369c2af56e75a26de2b50000b4125b
Taylor Arnett <tarnett1@mac.com>
2022-07-14 19:40:28 -0600

    add metadata struct type

--- neural_net.go
+++ neural_net.go
@@ -1,6 +1,14 @@
 package main
 
+import "time"
+
 type NeuralNet struct {
 	Version    int `json:"version"`
 	SampleSize int `json:"sample_size"`
+}
+
+type NeuralNetMetadata struct {
+	Version          int       `json:"version"`
+	SampleSize       int       `json:"sample_size"`
+	UpdatedTimestamp time.Time `json:"updated_timestamp"`
 }
