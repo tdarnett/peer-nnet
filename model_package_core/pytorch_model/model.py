4addf85ba6466ceeb2bb05ed6e7dfef3e98cd5f0
Milan Kordic <milan.kordic94@gmail.com>
2022-08-10 22:54:43 -0400

    add pytorch model and dataset files

---
+++ model_package_core/pytorch_model/model.py
@@ -0,0 +1,25 @@
+# very simple model to train on the MNIST dataset
+# this file is used to hold the architecture of any neural net
+
+import torch
+
+
+class Net(torch.nn.Module):
+    def __init__(self, input_size, hidden_units, number_of_classes):
+        super(Net, self).__init__()
+        # initialize the layer list
+        all_layers = []
+        # create the layers
+        for hidden_unit in hidden_units:
+            layer = torch.nn.Linear(input_size, hidden_unit)
+            all_layers.append(layer)
+            all_layers.append(torch.nn.ReLU())
+            input_size = hidden_unit
+        all_layers.append(torch.nn.Linear(hidden_units[-1], number_of_classes))
+        self.module_list = torch.nn.ModuleList(all_layers)
+
+    def forward(self, x):
+        # apply each layer to input
+        for f in self.module_list:
+            x = f(x)
+        return x
