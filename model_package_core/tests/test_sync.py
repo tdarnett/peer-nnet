2bc9df33c74703d94f6a7cf93ad4d9b04e39de63
Taylor Arnett <tarnett1@mac.com>
2022-09-07 19:40:51 -0600

    test new peer is synced to db

--- model_package_core/tests/test_sync.py
+++ model_package_core/tests/test_sync.py
@@ -2,6 +2,7 @@ import json
 
 from model_package_core.constants import METADATA_FILENAME, WEIGHT_FILENAME
 from model_package_core.sync import ModelMetadataSync
+from model_package_core.tests.conftest import create_peer_dir
 
 
 def test_new_db_syncs_all_peers(peers_db, peer_models_path):
@@ -31,11 +32,28 @@ def test_new_db_syncs_all_peers(peers_db, peer_models_path):
     assert len(models_to_train) == len(peers_in_peer_path) == len(peers_db)
 
 
-def test_syncs_new_peer(peers_db, peer_models_path):
-    # TODO
-    pass
+def test_syncs_new_peer_added_to_db(db_with_peers, peer_models_path):
+    # GIVEN a db with three peers
+    assert len(db_with_peers) == 3
+
+    # WHEN a new peer is added
+    new_peer_id = 'test_new_peer'
+    metadata = {"version": 1, "sample_size": 10, "last_updated": 1657843681}
+    create_peer_dir(peer_id=new_peer_id, peer_metadata=metadata, peers_path=peer_models_path)
+    # AND we sync the db
+    sync_commander = ModelMetadataSync(db=db_with_peers, peer_models=peer_models_path)
+    models_to_train = sync_commander._models_to_train()
+
+    # THEN models to train references the new peer only
+    assert len(models_to_train) == 1
+    assert new_peer_id in models_to_train
+    assert models_to_train[new_peer_id]['number_of_samples'] == 10
+
+    # AND the db also references the new peer
+    assert len(db_with_peers) == 4
+    assert db_with_peers[new_peer_id] == metadata
 
 
-def test_syncs_existing_peer_new_model_version(peers_db, peer_models_path):
+def test_syncs_existing_peer_new_model_version_updates_db(peers_db, peer_models_path):
     # TODO
     pass
