80845a8b44d8450feb250b3b107b2f6be1eba196
Taylor Arnett <tarnett1@mac.com>
2022-09-11 21:42:58 -0600

    house all shared fs files under a base shared directory

--- constants.go
+++ constants.go
@@ -13,9 +13,9 @@ func GetOrDefaultEnv(key string, fallback string) string {
 const WEIGHTS_FILENAME = "weights.h5"     // must be in sync with model manager weights filename constant!
 const METADATA_FILENAME = "metadata.json" // must be in sync with model manager metadata filename constant!
 
-var HOST_MODEL_WEIGHTS_PATH = GetOrDefaultEnv("HOST_MODEL_WEIGHTS_PATH", "model/"+WEIGHTS_FILENAME)
-var HOST_MODEL_METADATA_PATH = GetOrDefaultEnv("HOST_MODEL_METADATA_PATH", "model/"+METADATA_FILENAME)
+var HOST_MODEL_WEIGHTS_PATH = GetOrDefaultEnv("HOST_MODEL_WEIGHTS_PATH", "shared/model/"+WEIGHTS_FILENAME)
+var HOST_MODEL_METADATA_PATH = GetOrDefaultEnv("HOST_MODEL_METADATA_PATH", "shared/model/"+METADATA_FILENAME)
 
-var PEERS_MODELS_DIR = GetOrDefaultEnv("PEERS_MODELS_DIR", "peers/")
+var PEERS_MODELS_DIR = GetOrDefaultEnv("PEERS_MODELS_DIR", "shared/peers/")
 
 var DB_DIR = GetOrDefaultEnv("DB_DIR", "database/")
